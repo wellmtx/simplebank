ef1499a2a93477d967e0fe25b9d048c5dc759fd1
wellmtx <wellingtonsaraivasousa@gmail.com>
2023-12-30 02:16:00 -0300

    feat: :wrench: add viper config for envs

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/wellmtx/simplebank/api"
 	db "github.com/wellmtx/simplebank/db/sqlc"
+	"github.com/wellmtx/simplebank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://admin:admin@localhost:5432/simplebank?sslmode=disable"
-	serverAddress = "localhost:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
